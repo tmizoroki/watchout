6dc0b7adf90ef1f5602c0b90a3d245538f6396fb
tmizoroki <tmizoroki@gmail.com>
2015-08-28 14:36:42 -0700

    Add gameOptions and gameBoard

--- client/watchout.js
+++ client/watchout.js
@@ -1,2 +1,18 @@
-// start slingin' some d3 here.
+var gameOptions = {
+  width: 600,
+  height: 600,
+  numEnemies: 20
+};
+
+var gameStats = {
+  high: 0,
+  current: 0,
+  collisions: 0
+};
+
+var gameBoard = 
+  d3.select('body')
+  .append('svg')
+  .attr('width', gameOptions.width)
+  .attr('height', gameOptions.height)
 
