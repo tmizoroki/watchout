69279e60fa1e56034ceceb48e78176588d6d2b68
tmizoroki <tmizoroki@gmail.com>
2015-08-28 14:48:25 -0700

    Add enemy and player constructor classes and create instances of enemies and the player

--- client/watchout.js
+++ client/watchout.js
@@ -16,3 +16,22 @@ var gameBoard =
   .attr('width', gameOptions.width)
   .attr('height', gameOptions.height)
 
+var Enemy = function(id) {
+  this.id = id;
+  this.x = Math.random() * gameOptions.width;
+  this.y = Math.random() * gameOptions.height;
+}
+
+var enemies = [];
+
+for (var i = 0; i < gameOptions.numEnemies; i++) {
+  enemies.push(new Enemy(i));
+}
+
+var Player = function() {
+  this.x = gameOptions.width / 2;
+  this.y = gameOptions.height / 2;
+};
+
+var players = [];
+players.push(new Player);
